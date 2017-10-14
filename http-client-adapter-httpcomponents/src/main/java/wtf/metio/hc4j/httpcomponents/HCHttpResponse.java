0e8314217c438ce4bf6d76bc477604397cb35102
Sebastian Hoß <mail@shoss.de>
2017-10-14 22:35:07 +0200

    license header

--- http-client-adapter-httpcomponents/src/main/java/wtf/metio/hc4j/httpcomponents/HCHttpResponse.java
+++ http-client-adapter-httpcomponents/src/main/java/wtf/metio/hc4j/httpcomponents/HCHttpResponse.java
@@ -1,25 +1,31 @@
+/*
+ * This file is part of http-client-adapter. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of http-client-adapter,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
 package wtf.metio.hc4j.httpcomponents;
 
 import wtf.metio.hc4j.HttpResponse;
 
 final class HCHttpResponse implements HttpResponse {
 
-  private String body;
-  private int    statusCode;
+    private final String body;
+    private final int    statusCode;
 
-  public HCHttpResponse(String body, int statusCode) {
-    this.body = body;
-    this.statusCode = statusCode;
-  }
+    public HCHttpResponse(final String body, final int statusCode) {
+        this.body = body;
+        this.statusCode = statusCode;
+    }
 
-  @Override
-  public String getBody() {
-    return body;
-  }
+    @Override
+    public String getBody() {
+        return body;
+    }
 
-  @Override
-  public int getStatusCode() {
-    return statusCode;
-  }
+    @Override
+    public int getStatusCode() {
+        return statusCode;
+    }
 
 }
